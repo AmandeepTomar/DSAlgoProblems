1baa2974debcb0f4cc67a5f10897b84e46d304ed
TOMAR Amandeep <amandeeptomar005@gmail.com>
2022-01-18 10:35:01 +0530

    - Update Problem description

---
+++ src/ArraysProblem/ConvertBinaryToDecimal.java
@@ -0,0 +1,26 @@
+package ArraysProblem;
+
+import java.util.Arrays;
+
+public class ConvertBinaryToDecimal {
+    public static void main(String[] args) {
+        String binary="0111111111111111111111111111111111111111111111111111111111111111";
+        String binary2="001101"; // 13
+
+        findDecimal(binary2.toCharArray());
+        findDecimal(binary.toCharArray());
+
+    }
+
+    private static void findDecimal(char[] arr) {
+        double decimal=0;
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+            if (arr[i]=='1'){
+                decimal=decimal+Math.pow(2,(arr.length-1-i));
+                System.out.println(decimal+" power "+Math.pow(2,(arr.length-1-i))+" i "+(arr.length-1-i));
+            }
+        }
+        System.out.println("Decimal values id "+(long)decimal+" of binary "+ Arrays.toString(arr));
+    }
+}
---
+++ src/ArraysProblem/FindMaximumOccuranceInString.java
@@ -0,0 +1,49 @@
+package ArraysProblem;
+
+
+import java.util.HashMap;
+
+/**
+ * you Have given string you need to print the String as the Occurrence in sequence
+ *
+ * Input aabbccddeee;
+ * Output e -> 3 times
+ *
+ * Input aabbcc
+ * output a -> 2times
+ * */
+
+public class FindMaximumOccuranceInString {
+
+    public static void main(String[] args) {
+        String s="aabbccddee";
+
+        solution(s);
+    }
+
+    private static void solution(String s){
+        int max=0;
+        String result="";
+        HashMap<Character,Integer> map=new HashMap();
+        for (int i = 0; i <s.length(); i++) {
+            char key=s.charAt(i);
+            if (map.containsKey(key)){
+                int value=map.get(key)+1;
+                map.put(key,value);
+            }else {
+                map.put(key,1);
+            }
+        }
+
+        for(Character ch:map.keySet()){
+            if (max<map.get(ch)){
+                max=map.get(ch);
+                result=ch.toString();
+            }
+
+        }
+
+        System.out.println("Char is "+result+" Times "+max);
+
+    }
+}
