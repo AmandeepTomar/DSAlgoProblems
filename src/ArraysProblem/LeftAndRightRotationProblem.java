42a2d5810c9c6564a60ddea823bb394445cce40c
TOMAR Amandeep <amandeeptomar005@gmail.com>
2022-02-03 23:11:00 +0530

    - Added bitwise Operators definition

--- src/ArraysProblem/LeftAndRightRotationProblem.java
+++ src/ArraysProblem/LeftAndRightRotationProblem.java
@@ -4,22 +4,67 @@ import java.util.Arrays;
 
 public class LeftAndRightRotationProblem {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        int times=4;
-        rightRotationApproach1(arr,times);
+//        int arr[]={1,2,3,4,5};
+//        int times=4;
+//        rightRotationApproach1(arr,times);
+//
+//        int arr1[]={1,2,3,4,5};
+//        rightRotationBestApproach(arr1,times);
+//
+//
+//        int arr2[]={1,2,3,4,5};
+//        leftRotationApproach1(arr2,times);
+//
+//         int arr3[]={1,2,3,4,5};
+        //leftRotationBestApproach(arr3,times);
 
-        int arr1[]={1,2,3,4,5};
-        rightRotationBestApproach(arr1,times);
+        int [] nums={1,2,3,4,5,6,7};
+        rotate(nums,1);
 
 
-        int arr2[]={1,2,3,4,5};
-        leftRotationApproach1(arr2,times);
 
-         int arr3[]={1,2,3,4,5};
-        leftRotationBestApproach(arr3,times);
 
 
     }
+
+
+    public static void rotate(int[] nums, int k) {
+        // [1,2,3,4,5,6,7]
+        // right rotate -> [2,3,4,5,6,7,1] -> [2,3,4,5,6,7,1,2]->[3,4,5,6,7,1,2,3]
+
+        // left rotate
+        //-> [7,1,2,3,4,5,6]-> [6,7,1,2,3,4,5]->[5,6,7,1,2,3,4]
+//        for(int i=0;i<k;i++){
+//            int temp =nums[nums.length-1];
+//            for(int j=nums.length-1;j>0;j--){
+//                nums[j]=nums[j-1]; //arr[6]=arr[5];
+//            }
+//            nums[0]=temp;
+//        }
+
+        int arr[]=new int[nums.length];
+        int n=nums.length;
+        for (int i = 0; i < n ; i++) {
+            int newIndex=(i+k)%n;
+            System.out.println("new Index "+newIndex+"  CurrentIndex "+i);
+           // swap(nums,newIndex,i);
+            arr[newIndex]=nums[i];
+            System.out.println("Arrays is "+Arrays.toString(arr));
+        }
+
+    }
+
+    private static void swap(int[] arr, int newIndex , int current){
+        System.out.println("Before Swap--,"+"New Index is"+arr[newIndex]+"current"+arr[current]+" = "+Arrays.toString(arr));
+        int temp = arr[newIndex];
+        arr[newIndex]=arr[current];
+        arr[current] = temp;
+        System.out.println("After Swap--,"+"New Index is"+arr[newIndex]+"current"+arr[current]+" = "+Arrays.toString(arr));
+    }
+
+
+
+
 
     private static void rightRotationApproach1(int[] arr, int times) {
         // right rotation means shift one element to the right
@@ -71,6 +116,9 @@ public class LeftAndRightRotationProblem {
         }
         System.out.println("Left Rotation "+Arrays.toString(newArr)+" times "+times);
     }
+
+
+
 
 
 
---
+++ src/Test.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // As this one is int operation ,  it is like an
+    // 10/2 = 5 and 11/2 = 5.5 that is 5 in int
+
+    val a =10
+    val b=11
+    if (a/2==5){
+        println(" a is 10")
+    }
+
+    if (b/2==5){
+        println(" a is 11")
+    }
+
+}
